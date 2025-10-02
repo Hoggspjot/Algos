7e65bdc73fee5f4991acb60b43cff6bd872e7ea5
Hoggspjot <vingisaar@gmail.com>
2025-10-02 23:49:13 +0300

    Сортировка вставками

---
+++ src/main/java/october/day021025/InsertionSort.java
@@ -0,0 +1,34 @@
+package october.day021025;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        int[] array = new int[10];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(100);
+        }
+        System.out.println(Arrays.toString(array));
+        insertionSort(array);
+        System.out.println(Arrays.toString(array));
+
+
+    }
+
+
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+}
