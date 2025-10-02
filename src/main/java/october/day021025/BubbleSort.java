54d953056a72685d64b509620b0cd0eda220f803
Hoggspjot <vingisaar@gmail.com>
2025-10-02 23:43:10 +0300

    по курсу алго Сортировка выбором

---
+++ src/main/java/october/day021025/BubbleSort.java
@@ -0,0 +1,34 @@
+package october.day021025;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+
+        int[] array = new int[10];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(100);
+        }
+        System.out.println(Arrays.toString(array));
+        bubbleSort(array);
+        System.out.println(Arrays.toString(array));
+
+    }
+
+
+    public static void bubbleSort(int[] arr) {
+
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = arr.length - 1; j >= i; j--) {
+                if (arr[j-1] > arr[j]) {
+                    int temp = arr[j-1];
+                    arr[j-1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+    }
+}
--- src/main/java/october/day021025/SelectSort.java
+++ src/main/java/october/day021025/SelectSort.java
@@ -3,32 +3,33 @@ package october.day021025;
 import java.util.Arrays;
 import java.util.Random;
 
-public class Main {
+public class SelectSort {
     public static void main(String[] args) {
-
         Random random = new Random();
 
         int[] array = new int[10];
+
         for (int i = 0; i < array.length; i++) {
             array[i] = random.nextInt(100);
         }
+
         System.out.println(Arrays.toString(array));
-        bubbleSort(array);
+        selSort(array);
         System.out.println(Arrays.toString(array));
 
     }
 
 
-    public static void bubbleSort(int[] arr) {
+    public static void selSort(int[] arr) {
 
-        for (int i = 1; i < arr.length; i++) {
-            for (int j = arr.length - 1; j >= i; j--) {
-                if (arr[j-1] > arr[j]) {
-                    int temp = arr[j-1];
-                    arr[j-1] = arr[j];
-                    arr[j] = temp;
-                }
+        for (int i = 0; i < arr.length; i++) {
+            int min = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if(arr[j] < arr[min]) min = j;
             }
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
         }
     }
 }
