337bdd7f02c9401e458c27d6073d15bb1c2aa4dd
Hoggspjot <vingisaar@gmail.com>
2025-10-04 22:13:36 +0300

    Подсчет уникальных элементов

---
+++ src/main/java/october/day031025/Main.java
@@ -0,0 +1,32 @@
+package october.day031025;
+
+import java.util.HashMap;
+import java.util.Scanner;
+public class Main {
+    public static int uniqueValues(int[] arr){
+        int uniqueCount = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (map.containsKey(arr[i])) {
+                map.put(arr[i], map.get(arr[i]) + 1);
+            } else {
+                map.put(arr[i], 1);
+            }
+        }
+        uniqueCount = map.size();
+
+        return uniqueCount;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(uniqueValues(arr));
+    }
+}
+
+//Написать метод uniqueValues(), который определяет количество уникальных значений в одномерном массиве (возможны повторы).
