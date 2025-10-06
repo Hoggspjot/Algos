1c04b908c8347babd6c0fce96ac278898b03b893
Hoggspjot <vingisaar@gmail.com>
2025-10-06 23:11:43 +0300

    revert bubble sort

---
+++ src/main/java/october/day061025/Main2.java
@@ -0,0 +1,40 @@
+package october.day061025;
+
+import java.util.Scanner;
+public class Main2 {
+    public static void printArr(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+
+
+    public static void mySort(int[] arr){
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j + 1] > arr[j]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        mySort(arr);
+        printArr(arr);
+    }
+}
+
+
+//Написать метод mySort(), который упорядочивает исходный одномерный массив типа int по убыванию.
