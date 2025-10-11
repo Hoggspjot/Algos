61dd505b7b9c0a1b9c15ae83d4151920459c1f6f
Hoggspjot <vingisaar@gmail.com>
2025-10-11 22:44:47 +0300

    подсчет количества элементов

---
+++ src/main/java/october/day111025/task2/Main.java
@@ -0,0 +1,47 @@
+package october.day111025.task2;
+
+import java.util.Scanner;
+class Node{
+    String data;
+    Node next;
+    Node(String value) {
+        this.data = value;
+    }
+}
+public class Main {
+    public static Node createSLLFromString(String s) {
+        String[] strings = s.split(" ");
+        Node head = new Node(strings[0]);
+        Node current = head;
+        for (int i = 1; i < strings.length; i++) {
+            current.next = new Node(strings[i]);
+            current = current.next;
+        }
+        return head;
+    }
+
+    public static int countElemInSLL(Node head, int value) {
+        int count = 0;
+
+        Node current = head;
+        while (current.next != null) {
+            if (Integer.parseInt(current.next.data) == value) {
+                count++;
+            }
+            current = current.next;
+        }
+
+        return count;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int value = sc.nextInt();
+        sc.nextLine();
+        String s = sc.nextLine();
+        Node head = createSLLFromString(s);
+        System.out.println(countElemInSLL(head, value));
+    }
+}
+
+
+//Выведите количество элементов с значением value в односвязном списке.
